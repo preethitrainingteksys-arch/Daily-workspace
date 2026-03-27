1bd3b9508c5acfb6a07854d375f1009fd231f8d5
preethitrainingteksys <preethitrainingteksys@gmail.com>
2026-03-27 16:19:12 +0530

    Day12 Todo updated

--- Day12_JS_miniproject(TODO APP)/app.js
+++ Day12_JS_miniproject(TODO APP)/app.js
@@ -1,48 +1,94 @@
-let todos=JSON.parse(localStorage.getItem('todos')) || [];
-        function saveTodos()
-        {
-            localStorage.setItem('todos',JSON.stringify(todos));
-        }
-   
-    function addTodo()
-    {
-        const input =document.getElementById('todo-input');
-        const text=input.value.trim();
-        if(text==='') return;
-        todos.push({text,completed:false});
-        input.value='';
-        saveTodos();
-        renderTodos();
-    }
-    function deleteTodo(index)
-    {
-        todos.splice(index,1);
-        saveTodos();
-        renderTodos();
-    }
-    function toggleComplete(index)
-    {
-        todos[index].completed=!todos[index].completed;
-        saveTodos();
-        renderTodos();
-    }
-    function renderTodos()
-    {
-        const list =document.getElementById('todo-list');
-        list.innerHTML='';
-        todos.forEach((todo,index)=>
-        {
-            const li=document.createElement('li');
-            if(todo.completed)li.classList.add('completed');
-            li.innerHTML=`
-            <span>${todo.text}</span>
-            <div>
-                <button class ="YES" onclick="toggleComplete(${index})">YES</button>
-                <button class ="NO" onclick="deleteTodo(${index})">NO</button>
-            </div>
-            `;
-            list.appendChild(li);
-        });
-        
-    }
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+const errorDiv=document.getElementById('error');
+
+
+function addTodo() {
+    const input = document.getElementById('todo-input');
+    const text = input.value.trim();
+    const time = document.getElementById('time-input').value;
+
+    const selected = document.querySelector('input[name="priority"]:checked');
+    const priority = selected ? selected.value : "important";
+
+    if (text === '') return;
+
+    todos.push({
+        text,
+        time,
+        priority,
+        completed: false
+    });
+
+    input.value = '';
+    saveTodos();
     renderTodos();
+}
+function sortTodo() {
+    todos.sort((e1, e2) => {
+        return e1.text.localeCompare(e2.text);
+    })
+    console.log(todos);
+    renderTodos();
+}
+saveTodos();
+renderTodos();
+
+
+
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function deleteTodo(index) {
+    todos.splice(index, 1);
+    saveTodos();
+    renderTodos();
+}
+function validate() { 
+    errorDiv.style.display = (event.target.value.trim() !== '') ? 'none' : 'inline';
+}
+
+
+function toggleComplete(index) {
+    todos[index].completed = !todos[index].completed;
+    saveTodos();
+    renderTodos();
+}
+
+
+function renderTodos() {
+    const urgentList = document.getElementById('urgent-list');
+    const importantList = document.getElementById('important-list');
+
+   urgentList.innerHTML="";
+   importantList.innerHTML="";
+
+    todos.forEach((todo, index) => {
+        const li = document.createElement('li');
+
+        if (todo.completed) li.classList.add('completed');
+
+        li.innerHTML = `
+        <div class="task-left">
+            <span class="task-text">${todo.text}</span>
+            <span class="task-time">${todo.time || ""}</span>
+        </div>
+
+        <div>
+            <button class="YES" onclick="toggleComplete(${index})">✔</button>
+            <button class="NO" onclick="deleteTodo(${index})">✖</button>
+        </div>
+        `;
+
+        if (todo.priority === "urgent") {
+            li.style.borderLeft = "red";
+            urgentList.appendChild(li);
+        } else {
+            li.style.borderLeft = " orange";
+            importantList.appendChild(li);
+        }
+    });
+}
+
+
+renderTodos();
