ad949170ab1695c88d2a2da9cdb4a7b4e87bb99c
preethitrainingteksys <preethitrainingteksys@gmail.com>
2026-04-02 13:46:21 +0530

    react day-2code with status field

---
+++ Day-17 React notes app/notes-app/src/components/NoteForm.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+function NoteForm({addNote})
+{
+    const [note,setNote]=useState({
+        title:"my note",
+        status:"created"
+    });
+    const handleSubmit=(e)=>
+    {
+        e.preventDefault();
+       // if(!note.trim()) return;
+        addNote(note);
+       // setNote({});
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+            placeholder="Enter note"
+            value={note.title}
+            onChange={(e)=>setNote({
+                ...note,
+                title:e.target.value})}
+            />
+            <input value={note.status} onChange={(e)=>
+                setNote({
+                    ...note,
+                    status:e.target.value})}/ >
+            
+            <button>Add</button>
+        </form>
+    );
+}
+export default NoteForm;
---
+++ Day-17 React notes app/notes-app/src/components/NoteItem.js
@@ -0,0 +1,11 @@
+function NoteItem({note,deleteNote})
+{
+    return (
+        <li>
+            <span>{note.title}</span>
+            <span>{note.status}</span>
+            <button onClick ={()=>deleteNote(note.id)}>Delete</button>
+        </li>
+    );
+}
+export default NoteItem;
