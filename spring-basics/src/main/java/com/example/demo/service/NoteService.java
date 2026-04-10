237a5fb79d65254a88831619c4a501b63e782749
preethitrainingteksys <preethitrainingteksys@gmail.com>
2026-04-10 14:49:27 +0530

    create order with JPA

--- spring-basics/src/main/java/com/example/demo/service/NoteService.java
+++ spring-basics/src/main/java/com/example/demo/service/NoteService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
+import org.springframework.beans.factory.annotation.Autowired;
 //
 //import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Order1;
+import com.example.demo.repository.Order1Repository;
 
 //@Component
 //public class NoteService {
@@ -16,6 +18,8 @@ public class NoteService {
 //    public NoteService() {
 //        System.out.println("noteservice initialized");
 //    }
+	@Autowired
+	Order1Repository order1Repository;
 
 	public Order1 getOrder() {
 		Order1 order=new Order1();
@@ -25,8 +29,8 @@ public class NoteService {
 	}
 
 	public Integer addOrder(Order1 order1) {
-	
-		return 1;
+		order1Repository.save(order1);
+		return order1.getId();
 	}
 
 	
